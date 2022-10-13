e5d91f4d77e7df527a8e2188efa2d443301d77bc
Bashitapwa Michael <michaelbashitapwa52@gmail.com>
2022-10-13 20:33:31 +0300

    solved the question without using arrays

--- Task2/Task2.cpp
+++ Task2/Task2.cpp
@@ -1,27 +1,23 @@
 
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main() {
-    int arr[20] = {};
-    int i = 0, len;
+    string sequence;
 
-    do {
-        cin >> arr[i];
-        len = ++i;
-    } while (arr[i - 1] != 0);
+    cout << "Enter sequence: ";
+    cin >> sequence;
 
-    int pos = 0;
-    int maxz = arr[0];
-    for (i = 1; i <= len - 1; i++) {
-        maxz = max(maxz, arr[i]);
-    }
-
-    for (i = 0; i <= len - 1; i++) {
-        if (maxz == arr[i]) {
-            break;
+    int i = 1, pos_of_highest = 0;
+    while (int(sequence[i]) != 0)
+    {
+        if (int(sequence[i]) > int(sequence[pos_of_highest])) {
+            pos_of_highest = i;
         }
+        i++;
     }
 
-    cout << i << endl;
+    cout << pos_of_highest;
 }
--- Task2/c++ task.cpp
+++ Task2/c++ task.cpp
@@ -3,25 +3,41 @@
 using namespace std;
 
 int main() {
-    int arr[20] = {};
-    int i = 0, len;
+	string sequence;
 
-    do {
-        cin >> arr[i];
-        len = ++i;
-    } while (arr[i - 1] != 0);
+	cout << "Enter sequence: ";
+	cin >> sequence;
 
-    int pos = 0;
-    int maxz = arr[0];
-    for (i = 1; i <= len - 1; i++) {
-        maxz = max(maxz, arr[i]);
-    }
+	int i = 0, pos_of_highest = int(sequence[0]);
+	while (int(sequence[i]) != 0)
+	{
+		if (int(sequence[i]) > int(sequence[pos_of_highest])) {
+			pos_of_highest = i;
+		}
+		i++;
+	}
 
-    for (i = 0; i <= len - 1; i++) {
-        if (maxz == arr[i]) {
-            break;
-        }
-    }
+	cout << i;
 
-    cout << i << endl;
+	/*int arr[20] = {};
+	int i = 0, len;
+
+	do {
+		cin >> arr[i];
+		len = ++i;
+	} while (arr[i - 1] != 0);
+
+	int pos = 0;
+	int maxz = arr[0];
+	for (i = 1; i <= len - 1; i++) {
+		maxz = max(maxz, arr[i]);
+	}
+
+	for (i = 0; i <= len - 1; i++) {
+		if (maxz == arr[i]) {
+			break;
+		}
+	}
+
+	cout << i << endl;*/
 }
